6e604d55b962fe8ca740a68d72c58f000c65ff63
Enkhe Garmaev <garmaevenhe@gmail.com>
2023-04-09 18:16:07 +0800

    feat: add burger menu pets page

--- Shelter/assets/js/burger.js
+++ Shelter/assets/js/burger.js
@@ -5,3 +5,28 @@ document.addEventListener("DOMContentLoaded", function(){
         document.querySelector(".header").classList.toggle("open")
     })
 } )
+
+
+
+
+
+document.getElementById("menu").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.body.addEventListener('click', event => {
+    if (event._isClickWithInMenu) return;
+    document.querySelector(".header").classList.remove("open")
+});
+
+
+
+document.querySelector("nav").addEventListener('click', event => {
+    document.querySelector(".header").classList.remove("open")
+});
+
+document.querySelector("header__nav_items").addEventListener('click', event => {
+    document.querySelector(".header").classList.remove("open")
+});

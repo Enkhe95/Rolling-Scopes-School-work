f9261bf319f98209d613d9c44e35d3bd3e2a5425
Enkhe Garmaev <garmaevenhe@gmail.com>
2023-05-20 01:21:11 +0800

    feat: add cells 10*10

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+"use strict";
+const title = document.createElement("h1");
+title.setAttribute("class", "title");
+title.innerHTML = "Minesweeper";
+
+const page = document.createElement("div");
+page.setAttribute("class", "page");
+
+const box = document.createElement("div");
+box.setAttribute("class", "box");
+
+const body = document.getElementById("root");
+body.appendChild(title);
+body.appendChild(page);
+page.appendChild(box);
+
+startGame(10, 10, 10);
+function startGame(WIDTH, HEIGHT, BOMBS_COUNT) {
+  const cellsCount = WIDTH * HEIGHT;
+  box.innerHTML = "<button></button>".repeat(cellsCount);
+}

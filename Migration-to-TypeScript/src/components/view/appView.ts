a9bed81b1bdf765a700b742a19bf8e5b27ad1a15
Enkhe Garmaev <garmaevenhe@gmail.com>
2023-06-13 02:52:44 +0800

    feat: change appView.js on appView.ts

--- Migration-to-TypeScript/src/components/view/appView.ts
+++ Migration-to-TypeScript/src/components/view/appView.ts
@@ -2,18 +2,21 @@ import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
+    public drawNews(data: { articles?: unknown[] }) {
+        const values = data?.articles ?? [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
-        const values = data?.sources ? data?.sources : [];
+    public drawSources(data: { sources?: unknown[] }) {
+        const values = data?.sources ?? [];
         this.sources.draw(values);
     }
 }
